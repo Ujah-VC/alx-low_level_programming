c6119c41a4f44197d7be2ed35a70bb0dc258511b
Ujah-VC <chuks_victor2014@yahoo.com>
2022-06-16 14:17:29 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
