2c7479c0473063986b4f14d15c7079a84965d6f2
Ujah-VC <chuks_victor2014@yahoo.com>
2022-06-17 17:11:51 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,63 +1,38 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * isLower - Determines whether ascii is lowercase
- *
- * @c: Character
- *
- * Return: 1 if true, 0 if false
- */
-
-int isLower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
 
 /**
- * isDelimiter - Determines whether ascii is a delimiter
- *
- * @c: Character
- *
- * Return: 1 if true, 0 if false
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
  */
-
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[] = " \t\n,.!?\"(){}";
-
-	for (i = 0; i < 12; i++)
-		if (c == delimiter[i])
-			return (1);
-
-	return (0);
-}
-
-/**
- * cap_string - Capitalizes all words of a string
- *
- * @s: Input string
- *
- * Return: string with capitalized words
- */
-
 char *cap_string(char *s)
 {
-	char *ptr = s;
-	int foundDelimit = 1;
+	int i;
 
-	while (*s)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (isDelimiter(*s))
-			foundDelimit = 1;
-		else if (isLower(*s) && foundDelimit)
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
 		{
-			*s -= 32;
-			foundDelimit = 0;
+			s[i] = s[i] - 32;
+		}
+		else if ((s[0] >= 97 && s[0] <= 122))
+		{
+			s[0] = s[0] - 32;
 		}
 		else
-			foundDelimit = 0;
-		s++;
+		{
+			s[i] = s[i];
+		}
+		i++;
 	}
-	return (ptr);
+	return (s);
 }
